b24a372a7be1a2e4d296355c600c73e90c633004
shipleaf <shipleaf@shipleafui-MacBookPro.local>
2025-05-19 00:02:53 +0900

    로딩페이지 => 로그인페이지

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { fetchSessionInfo } from "./api/UserActivity";
 import { useEffect, useState } from "react";
 import LoginCaution from "./components/login/LoginCaution";
 import { useRouter } from "next/navigation";
-import { loginWithSessionId } from "./api/useSocialLoginAPI";
 
 export default function Home() {
   const [isLoginModal, setIsLoginModal] = useState(false);
@@ -28,29 +27,6 @@ export default function Home() {
     }
     // eslint-disable-next-line
   }, [isLoading, isError]);
-
-  useEffect(() => {
-    // eslint-disable-next-line
-    const handler = (event: any) => {
-      try {
-        const { sessionId } = event.detail;
-        if (!sessionId) {
-          return;
-        }
-        loginWithSessionId(sessionId);
-        router.push("/home");
-      } catch (error) {
-        console.error("❌ sessionReceived 이벤트 처리 중 오류:", error); // TODO: 소셜 로그인 오류 모달처리
-      }
-    };
-
-    window.addEventListener("sessionReceived", handler);
-
-    return () => {
-      window.removeEventListener("sessionReceived", handler);
-    };
-    // eslint-disable-next-line
-  }, []);
 
   if (isLoading) {
     return (
